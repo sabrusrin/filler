4f6773044c4948ed7117bf43953ec81f9f1d20a0
sabrusrin <rusua967@gmail.com>
2019-05-26 22:37:22 +0300

    gkd

---
+++ filler/inc/filler.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   filler.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chermist <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/26 22:06:55 by chermist          #+#    #+#             */
+/*   Updated: 2019/05/26 22:36:12 by chermist         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FILLER_H
+#define FILLER_H
+
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <string.h>
+
+# include "libft.h"
+# include "get_next_line.h"
+
+#endif
