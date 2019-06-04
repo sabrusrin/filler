c0f7a4b65a92c8a55c0763c4e343285af829e1ea
sabrusrin <rusua967@gmail.com>
2019-06-05 00:09:55 +0300

    nothing

--- src/filler.c
+++ src/filler.c
@@ -6,7 +6,7 @@
 /*   By: chermist <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/25 20:16:34 by chermist          #+#    #+#             */
-/*   Updated: 2019/05/28 23:24:23 by chermist         ###   ########.fr       */
+/*   Updated: 2019/06/05 00:09:39 by chermist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	parse(t_map *in)
 {
 	char	*line;
 
-	while (get_next_line(0,  &line))
+	while (get_next_line(1,  &line))
 	{
 		if (ft_strstr(line, "$$$"))
 			save_data(0, in, line);
