c5dd6d7a8bed58161aed9904168ab59d86d225c2
Cristopher Hermiston <chermist@at-o8.21-school.ru>
2019-08-11 17:26:05 +0300

    in process of making an approach algorithm

--- src/dijkstra_map.c
+++ src/dijkstra_map.c
@@ -6,7 +6,7 @@
 /*   By: chermist <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/29 21:57:25 by chermist          #+#    #+#             */
-/*   Updated: 2019/08/11 00:29:54 by chermist         ###   ########.fr       */
+/*   Updated: 2019/08/11 17:23:14 by chermist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,11 +50,11 @@ void	distance(t_game *in, int x, int y, int flag)
 	ye = -1;
 	while (++ye < in->board.y && (xe = -1))
 		while (++xe < in->board.x)
-			if (in->heat_map[ye][xe][flag] == -flag)
-				val = (!val || val > ft_sqrt((ye - y) * (ye - y)
-					+ (xe - x) * (xe - x)))
-				? ft_sqrt((ye - y) * (ye - y)
-					+ (xe - x) * (xe - x)) : val;
+			if (in->heat_map[ye][xe][flag] == -flag)// multiplied here by 10
+				val = (!val || val > ft_sqrt(10 * ((ye - y) * (ye - y)
+					+ (xe - x) * (xe - x))))
+				? ft_sqrt(10 * ((ye - y) * (ye - y)
+					+ (xe - x) * (xe - x))) : val;
 	in->heat_map[y][x][flag] = val;
 }
 
@@ -67,13 +67,18 @@ void	fill_cell(t_game *in)
 	y = -1;
 	while (++y < in->board.y && (x = -1))
 		while (++x < in->board.x)
+		{
+			fcost = in->heat_map[y][x];
 			if (in->heat_map[y][x][0] == 1)
 			{
 				distance(in, x, y, 0);
 				distance(in, x, y, 1);
-				fcost = in->heat_map[y][x];
 				fcost[2] = fcost[0] + fcost[1];
 			}
+			else
+				fcost[2] = 0;
+
+		}
 }
 
 void	heat_map(t_game *in)
@@ -100,4 +105,45 @@ void	heat_map(t_game *in)
 	}
 	locate_players(in);
 	fill_cell(in);
+	ft_putstr_fd("|0|\n", in->fd);
+	y = -1;
+	while (++y < in->board.y)
+	{
+		x = -1;
+		while (++x < in->board.x)
+		{
+			ft_putnbr_fd((in->heat_map)[y][x][0], in->fd);
+			ft_putchar_fd(' ', in->fd);
+		}
+		ft_putchar_fd('\n', in->fd);
+	}
+	ft_putstr_fd("|0|\n", in->fd);
+	ft_putstr_fd("|1|\n", in->fd);
+	y = -1;
+	while (++y < in->board.y)
+	{
+		x = -1;
+		while (++x < in->board.x)
+		{
+			ft_putnbr_fd((in->heat_map)[y][x][1], in->fd);
+			ft_putchar_fd(' ', in->fd);
+		}
+		ft_putchar_fd('\n', in->fd);
+	}
+	ft_putstr_fd("|1|\n", in->fd);
+	ft_putstr_fd("|2|\n", in->fd);
+	y = -1;
+	while (++y < in->board.y)
+	{
+		x = -1;
+		while (++x < in->board.x)
+		{
+			ft_putnbr_fd((in->heat_map)[y][x][2], in->fd);
+			ft_putchar_fd(' ', in->fd);
+		}
+		ft_putchar_fd('\n', in->fd);
+	}
+	ft_putstr_fd("|2|\n", in->fd);
+
+
 }
--- src/player.c
+++ src/player.c
@@ -6,13 +6,13 @@
 /*   By: chermist <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/04 00:38:01 by chermist          #+#    #+#             */
-/*   Updated: 2019/08/11 00:29:53 by chermist         ###   ########.fr       */
+/*   Updated: 2019/08/11 17:25:29 by chermist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "filler.h"
 
-void	check_map(t_game *in)
+int		check_map(t_game *in)
 {
 	int	x;
 	int	y;
@@ -22,13 +22,21 @@ void	check_map(t_game *in)
 	y = -1;
 	while (++y < in->board.y)
 		while (++x < in->board.x)
-			if (in->heat_map[y][x][1] == 1)
+		{
+			if (in->heat_map[y][x][2] <= 7)
+				return (2);
+			else
+				return (1);
+		}
+	return (0);
 }
 
 void	is_placeable(t_game *in)
 {
-	void
+	// 1 * should overlap
+	// 
 }
+
 void	size_fig(t_game *in)
 {
 	int	x;
@@ -49,7 +57,7 @@ void	size_fig(t_game *in)
 	}
 }
 
-int		approach(t_game *in)
+void	approach(t_game *in)
 {
 	int	x;
 	int	y;
@@ -59,28 +67,35 @@ int		approach(t_game *in)
 	y = -1;
 	fcost = 0;
 	hcost = 0;
-	while (++y < in->board.y)
-	{
-		x = -1;
+	while (++y < in->board.y && (x = -1))
 		while (++x < in->board.x)
-		{
-			if ((in->heat_map[y][x][0] > 0) &&
-				(in->heat_map[y][x][1] == 1) &&
-				(!fcost || fcost >= in->heat_map[y][x][2]))
-				if (fcost == in->heat_map[y][x][2])
-				//if found the same fcost => check for hcost
-				fcost = in->he
-		}
-	}
+			if ((in->heat_map[y][x][2] > 0) && // check for figure
+				(in->heat_map[y][x][1] == 3 ||
+			   	in->heat_map[y][x][1] == 4) && // check how close i'm to player
+				(!fcost || fcost >= in->heat_map[y][x][2])) // check if i should consider this cell
+				if ((fcost > in->heat_map[y][x][2]) ||
+					((fcost == in->heat_map[y][x][2]) &&
+					(in->heat_map[in->p.p_y][in->p.p_x][0] >
+					 in->heat_map[y][x][0])) &&
+					is_placeable(in, y, x))
+					{
+						in->p.p_y = y;
+						in->p.p_x = x;
+						fcost = in->heat_map[y][x][2];
+					}
+	place_fig(in);
 }
 
 void	player(t_game *in)
 {
-	static int	approached = 0;
+	int	strategy;
 
-	check_map(in);
-	if (!approached)// !approached
-		approached = approach(in);
-	else // approached
+	strategy = check_map(in);
+	size_fig(in);
+	if (strategy == 1)// !approached
+		approach(in);
+	else if (strategy == 2)// approached
 		surround(in);
+	else
+		fill(in);
 }
